186fb2b9596fdb5815b9c3017ea6278b3e1201a3
Sparsh101AI <Sparshpatel6165@gmail.com>
2022-12-28 14:09:50 -0700

    added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,17 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
+
         <Row className="align-items-center">
         
           <Col size={12} sm={6}>
           </Col>
+
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/sparsh-patel-54762b186/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/Sparsh101AI"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/_sparsh.patel_/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Thank you for attention</p>
           </Col>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,8 +3,7 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
+
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -38,35 +37,35 @@ export const Skills = () => {
                         <p>Just a little overview...</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter3}  />
                                 <h5>C++ / C</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter2}  />
                                 <h5>Python</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter3}  />
                                 <h5>AR Core</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter1}  />
                                 <h5>UI / UX</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter2} />
                                 <h5> Javascript</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter3}  />
                                 <h5> Teamwork</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter1}  />
                                 <h5> Work Ethic</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter3}  />
                                 <h5>Adaptibility</h5>
                             </div>
                         </Carousel>
