e42e72e1bb0e2ff4b076839fc06bb4c547519cf0
Sparsh101AI <Sparshpatel6165@gmail.com>
2023-01-08 11:37:35 -0700

    removed filter tab section from projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,9 +23,7 @@ export const Projects = () => {
                 <p>Below are some of my highlighted projects, simply click on the pictire to learn more. The rest can be found on github.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
+                    
                     {/* <Nav.Item>
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
                     </Nav.Item>
