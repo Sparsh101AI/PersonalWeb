cbb887457086c17e2f5ae9e926d129ca7295f08e
Sparsh Patel <48163939+Sparsh101AI@users.noreply.github.com>
2023-07-02 17:49:26 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner />
+      <Banner /> //change banner
       <Aboutme/>
       <Skills />
       <Projects />
