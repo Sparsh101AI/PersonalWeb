aba1045a84738e04ade661624a6f02cd469c5ce3
Sparsh101AI <Sparshpatel6165@gmail.com>
2023-07-13 18:24:11 -0400

    added banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [, setIndex] = useState(1);
-  const toRotate = ["World", "Sekai", "Prithvi", "Shìjiè", "Monde"];
+  const toRotate = ["Street", "Dreams", ""];
   const period = 1000;
 
 
@@ -60,8 +60,7 @@ export const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline"> I'm Sparsh <span class="wave">👋</span></span>
-                  <h1>{`Welcome to my `} <span className="txt-rotate" dataPeriod="100" data-rotate='[]'><span className="wrap">{text}</span></span></h1>
-
+                  <h2>Welcome to my little street.</h2>
 
                 </div>}
             </TrackVisibility>
@@ -70,6 +69,7 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+
                   <img src={headerImg} alt="Header Img" />
                 </div>}
             </TrackVisibility>
