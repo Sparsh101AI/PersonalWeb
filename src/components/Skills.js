c5f253adb76ab4aad9185497624e4297e356abbd
Sparsh101AI <Sparshpatel6165@gmail.com>
2023-07-16 18:52:54 -0400

    updated changes for  now

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,83 +1,23 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import './Footer.css';
 
-import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
 
-  return (
-    <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills!</h2>
-                        <p>Just a little overview...</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div >
-                                <img src={meter3}  alt=""/>
-                                <h5>C++ / C</h5>
-                            </div>
-                            <div >
-                                <img src={meter2} alt="" />
-                                <h5>Python</h5>
-                            </div>
-                            <div >
-                                <img src={meter3}  alt=""/>
-                                <h5>AR Core</h5>
-                            </div>
-                            <div >
-                                <img src={meter1} alt="" />
-                                <h5>Autodesk & Fusion 360</h5>
-                            </div>
-                            <div >
-                                <img src={meter1} alt="" />
-                                <h5>UI / UX</h5>
-                            </div>
-                            <div >
-                                <img src={meter2}alt="" />
-                                <h5> Javascript</h5>
-                            </div>
-                            <div >
-                                <img src={meter3} alt="" />
-                                <h5> Teamwork</h5>
-                            </div>
-                            <div >
-                                <img src={meter1} alt="" />
-                                <h5> Work Ethic</h5>
-                            </div>
-                            <div >
-                                <img src={meter3} alt="" />
-                                <h5>Adaptibility</h5>
-                            </div>
-                        </Carousel>
+
+
+    return (
+        <section className="skill" id="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-bx wow zoomIn">
+                            <h2>Thoughts Garden</h2>
+                            <p>🚧This page is currently under construction. Stay tuned for updates on some intresting things.🚧</p>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <img className="background-image-left" src={colorSharp} alt="" />
-    </section>
-  )
+        </section>
+    );
 }
